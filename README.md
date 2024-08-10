6554059f2c13e088a35837510c8cc7785c3d505f
Zeeshan Ahmad <zishanahmad114477@gmail.com>
2024-08-11 03:04:29 +0500

    flutter update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# invidio
+# invideolearning
 
 A new Flutter project.
 
--- android/app/src/main/kotlin/com/example/invideolearning/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/invideolearning/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.invidio
+package com.example.invideolearning
 
 import io.flutter.embedding.android.FlutterActivity
 
